92021fcb014760f6d5a07bd87da08963e5f67e8e
Yuri Pourre <yuriamayuri@gmail.com>
2014-05-13 01:22:11 -0300

    Minor Changes

--- Sonat/src/br/com/etyllica/sonat/ChatClient.java
+++ Sonat/src/br/com/etyllica/sonat/ChatClient.java
@@ -5,7 +5,10 @@ import br.com.etyllica.sonat.desktop.DesktopClient;
 public class ChatClient {
 
 	public static void main(String[] args) throws Exception {
-		new DesktopClient("127.0.0.1", 8000).init();
+		
+		new DesktopClient("127.0.0.1", 8080).init();
+		//new DesktopClient("10.42.0.1", 8080).init();
+		
 	}
 
 }
--- Sonat/src/br/com/etyllica/sonat/ChatServer.java
+++ Sonat/src/br/com/etyllica/sonat/ChatServer.java
@@ -5,7 +5,7 @@ import br.com.etyllica.sonat.server.Server;
 public class ChatServer {
 
 	public static void main(String[] args) throws Exception {
-		new Server(8000).init();
+		new Server(8080).init();
 	}
 	
 }
