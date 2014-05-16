ddbe3dc792d1ce54de64a91506d4134509aeae8d
Yuri Pourre <yuriamayuri@gmail.com>
2014-05-15 22:42:31 -0300

    Change default port to 4444

--- Sonat/src/br/com/etyllica/sonat/ChatServer.java
+++ Sonat/src/br/com/etyllica/sonat/ChatServer.java
@@ -5,9 +5,11 @@ import br.com.etyllica.sonat.adapter.netty.server.NettyServer;
 
 public class ChatServer {
 
+	private static final int DEFAULT_PORT = 4444;
+	
 	public static void main(String[] args) throws Exception {
-		new NettyServer(8081).init();
-		//new MinaServer(8081).init();
+		new NettyServer(DEFAULT_PORT).init();
+		//new MinaServer(DEFAULT_PORT).init();
 	}
 	
 }
