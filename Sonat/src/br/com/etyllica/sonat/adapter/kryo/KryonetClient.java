de930683bd9472e1f959e78df06bfe065db1a4fe
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-09 23:41:38 -0300

    Add kryonet adapters

---
+++ Sonat/src/br/com/etyllica/sonat/adapter/kryo/KryonetClient.java
@@ -0,0 +1,44 @@
+package br.com.etyllica.sonat.adapter.kryo;
+
+
+import com.esotericsoftware.kryonet.Client;
+
+import br.com.etyllica.sonat.client.ClientImpl;
+
+public class KryonetClient extends ClientImpl {
+
+	private static final int MAXIMUM_WAIT_TIME = 5000;
+	
+	protected Client client;
+	
+	public KryonetClient(String host, int port) {
+		super(host, port);
+	}
+
+	@Override
+	public void init() {
+		client = new Client();
+	}
+
+	@Override
+	public void finish() {
+		client.stop();
+	}
+
+	@Override
+	public void sendTCP(String message) {
+		client.sendTCP(message);
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void connect() throws Exception {
+		client.start();
+		client.connect(MAXIMUM_WAIT_TIME, host, tcpPort);
+	}
+
+}
